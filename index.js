119bb2a72c4f0c9e15269aa542d494a3ddc7a574
Valerie Hill <itsvhill@gmail.com>
2020-05-10 20:32:11 -0500

    wrote prompt questions

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
-const questions = [
+var inquirer = require("inquirer");
+var fs = require('fs');
+var markdown = require('./generateMarkdown.js');
 
-];
-
+inquirer.prompt([
+    {
+        type: "input",
+        name: "Project Title",
+        message: "What is your project title?"
+    },
+    {
+        type: "checkbox",
+        message: "Check which badges you will include:",
+        name: "Badges",
+        choices: [
+          "HTML", 
+          "CSS", 
+          "JavaScript", 
+          "MySQL"
+        ]
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Write a description of your project:"
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "Write a Table of Contents:"
+    },
+    {
+        type: "input",
+        name: "Installation Instructions",
+        message: "Write your installation instructions:"
+    },
+    {
+        type: "input",
+        name: "Usage Intructions",
+        message: "Write your usage instructions:"
+    },
+    {
+        type: "input",
+        name: "Licenses",
+        message: "Include any licensing information:"
+    },
+    {
+        type: "input",
+        name: "Contributors",
+        message: "Include any contributor information:"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Include any tests you've written for your application:"
+    },
+    {
+        type: "input",
+        name: "Questions and Feedback",
+        message: "Include any links or resources for feedback or questions on your application:"
+    },
+  ])
+    
 function writeToFile(fileName, data) {
-}
+    const fileName = "ReadMe.md"
+    if (err) {
+        return console.log(err);
+        }
 
-function init() {
+        console.log("Success!");
+    
+    }
 
-}
+    function init(markdown) {
 
-init();
+    }
+
+    init()
